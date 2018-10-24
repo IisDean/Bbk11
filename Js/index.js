874807d8cba5f30ce0003b19797f744ea2159a1b
Liu min <781775946@qq.com>
2018-10-24 12:02:06 +0800

    1

--- Js/index.js
+++ Js/index.js
@@ -2,6 +2,27 @@
  * Created by IisDean on 2018/10/23.
  */
 
+var setTimer;
+
+function dropGiftBox(){
+    var num = 0;
+    var $wrap = $(".page-container");
+    setTimer = setInterval(function(){
+        if( num >= 101 )clearInterval(setTimer);
+        var left = Math.ceil( Math.random() * (90 - 10) + 10  );
+        var h = $wrap.height() + 100;
+        var $giftBox = $('<div class="ui-abs ui-full-img gift-box" style="left: '+ left +'%;"><img src="Images/gift_box.png"></div>');
+        $wrap.append($giftBox);
+        $giftBox.animate({
+            'top': h+'px'
+        },3000,function(){
+            $giftBox.remove();
+        });
+        num++;
+    },300);
+}
+dropGiftBox();
+
 //抽奖
 $(".J-lottery-btn").on("click",function () {
     $(".J-lottery-pop").show();
@@ -21,3 +42,4 @@ $(".J-lotter-submit").on("click",function(){
         }
     });
 });
+
